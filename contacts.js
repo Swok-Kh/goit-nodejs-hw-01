695b9df7975eb529fb74cbcc95e5e1b3371df4b3
Serhii Demchenko <dem.s.vl@gmail.com>
2021-04-06 10:11:34 +0300

    add an error handler(on process.exit), add function for work with db

--- contacts.js
+++ contacts.js
@@ -1,6 +1,67 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs')
+const path = require('path')
 
-const contactsPath = path.parse("./db/contacts.json");
+const contactsPath = path.normalize('./db/contacts.json')
 
-module.exports = contactsPath;
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  fs.readFile(contactsPath, (err, data) => {
+    if (err) {
+      process.exit(1000)
+    }
+    console.table(JSON.parse(data.toString()))
+  })
+}
+
+function getContactById(contactId) {
+  fs.readFile(contactsPath, (err, data) => {
+    if (err) {
+      process.exit(1000)
+    }
+    const contacts = JSON.parse(data.toString())
+    const foundContact = contacts.find(({ id }) => id === contactId)
+    if (foundContact) console.table([foundContact])
+    else process.exit(1002)
+  })
+}
+
+function removeContact(contactId) {
+  fs.readFile(contactsPath, (err, data) => {
+    if (err) {
+      process.exit(1000)
+    }
+    const contacts = JSON.parse(data.toString())
+    const filteredContacts = contacts.filter(({ id }) => id !== contactId)
+    if (contacts.length === filteredContacts.length) {
+      process.exit(1002)
+    }
+    fs.writeFile(contactsPath, JSON.stringify(filteredContacts), err => {
+      if (err) {
+        process.exit(1001)
+      }
+      console.log(
+        `Contact with id ${contactId} was deleted. Saved successfully!`
+      )
+    })
+  })
+}
+
+function addContact(name, email, phone) {
+  fs.readFile(contactsPath, (err, data) => {
+    if (err) {
+      process.exit(1000)
+    }
+    const contacts = JSON.parse(data.toString())
+    const id = contacts[contacts.length - 1].id + 1
+    contacts.push({ id, name, email, phone })
+
+    fs.writeFile(contactsPath, JSON.stringify(contacts), err => {
+      if (err) {
+        process.exit(1001)
+      }
+      console.log('Contact was added. Saved successfully!')
+    })
+  })
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact }
--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-const contactsPath = require("./contacts");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact
+} = require('./contacts')
 
-console.log("Hello");
+// listContacts()
+// getContactById(4)
+// removeContact(3)
+addContact('Serhii', 'dem@com.ua', '333-333-333')
+
+process.on('exit', code => {
+  switch (code) {
+    case 0:
+      console.log('Goodbye!')
+      return
+    case 1000:
+      console.log(`Read file error. Exit code: ${code}`)
+      return
+    case 1001:
+      console.log(`Write file error. Exit code: ${code}`)
+      return
+    case 1002:
+      console.log(`Contact not found. Exit code: ${code}`)
+      return
+    default:
+      console.log(`Unknown error code. Exit code: ${code}`)
+  }
+})
